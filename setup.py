e80228c77d5b4d099ecaa4e52a035d9f4906c8dd
gw <likelightguang@163.com>
2018-01-07 14:24:13 +0800

    nester.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
 		name 		='nester',
-		version 	='1.1.1',
+		version 	='1.1.2',
 		py_modules	=['nester'],
 		author		='gw',
 		author_email='likelightguang@163.com',
