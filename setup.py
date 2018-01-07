fec425d96c4f20a521ef10dd0492a4831f769671
gw <likelightguang@163.com>
2018-01-07 14:06:08 +0800

    nester.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name 		='nester',
+		version 	='1.1.1',
+		py_modules	=['nester'],
+		author		='gw',
+		author_email='likelightguang@163.com',
+		url			='https://github.com/A-light/PyPi',
+		description ='A simple printere of nested lists',
+	)
