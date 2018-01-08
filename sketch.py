67b4aa77a8290d70b91e4d26dd43033f480273c9
gw <likelightguang@163.com>
2018-01-08 15:10:01 +0800

    sketch.py

--- sketch.py
+++ sketch.py
@@ -6,16 +6,16 @@ man=[]
 other=[]
 try:
 	with open('sketch.txt') as data:
-	for line in data:
-		try:
-			(role,line_spoken)=line.split(':',1)
-			line_spoken=line_spoken.strip()
-			if role=='Man':
-				man.append(line_spoken)
-			elif role=='Other Man':
-				other.append(line_spoken)
-		except ValueError as err:
-			pass
+		for line in data:
+			try:
+				(role,line_spoken)=line.split(':',1)
+				line_spoken=line_spoken.strip()
+				if role=='Man':
+					man.append(line_spoken)
+				elif role=='Other Man':
+					other.append(line_spoken)
+			except ValueError as err:
+				pass
 except IOError:
 	print('The data file is missing')
 try:
