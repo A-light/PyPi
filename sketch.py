6adfa472161f369977325785fe36358d8dcb3ae5
gw <likelightguang@163.com>
2018-01-08 15:08:45 +0800

    sketch.py

--- sketch.py
+++ sketch.py
@@ -1,17 +1,28 @@
 import os
 path=os.getcwd()
 path=path+"/HeadFirstPython/chapter3"
+os.chdir(path)
+man=[]
+other=[]
 try:
-	data=open('sketch.txt')
+	with open('sketch.txt') as data:
 	for line in data:
 		try:
 			(role,line_spoken)=line.split(':',1)
-			print(role,end='')
-			print(' said:',end='')
-			print(line_spoken,end='')
-		except ValueError:
+			line_spoken=line_spoken.strip()
+			if role=='Man':
+				man.append(line_spoken)
+			elif role=='Other Man':
+				other.append(line_spoken)
+		except ValueError as err:
 			pass
-	data.close()
 except IOError:
 	print('The data file is missing')
-
+try:
+	with open("man_data.out",'w') as man_data,open("other_data.out",'w') as other_data:
+		print(man,file=man_data)
+		print(other,file=other_data)
+except IOError as err:
+	pirnt("The out file is missing"+str(err))
+print(man)
+print(other)
