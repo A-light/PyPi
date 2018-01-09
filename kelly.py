43c352c0b03e21fcb04fa4f85262b04870f5541c
gw <likelightguang@163.com>
2018-01-09 15:07:27 +0800

    kelly.py

--- kelly.py
+++ kelly.py
@@ -7,22 +7,37 @@ james=[]
 sarah=[]
 julie=[]
 mikey=[]
+jsjm=[]
 def fileTolist(file):
 	listName=[]
 	try:
 		with open(file) as data:
 			for line in data:
-				temp=line.split(',')
-				for list_item in temp:
-					listName.append(list_item.strip())
+				listName=line.strip().split(',')
 	except IOError as err:
 		print("File is missing",str(err))
 	return listName
+
+def santize(time_string):
+	if time_string.find('-')!=-1:
+		splitter='-'
+	elif time_string.find(':')!=-1:
+		splitter=':'
+	else: 
+		return time_string
+	(mins,secs)=time_string.split(splitter)
+	return (mins+'.'+secs)
+
 names=["james.txt","mikey.txt","julie.txt","sarah.txt"]
 for names_item in names:
 	print(names_item)
+	times=[]
 	data=fileTolist(names_item)
-	nester.print_list(data)
+	for item in data:
+		times.append(santize(item))
+	jsjm.append(times)
+	#nester.print_list(data)
+print(jsjm)
 
 			
 
