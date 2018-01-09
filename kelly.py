af8abcceb901550fbf880bb702d58d56cf5bed1f
gw <likelightguang@163.com>
2018-01-09 23:40:39 +0800

    kelly.py

--- kelly.py
+++ kelly.py
@@ -33,13 +33,29 @@ for names_item in names:
 	print(names_item)
 	times=[]
 	data=fileTolist(names_item)
-	for item in data:
-		times.append(santize(item))
+	#for item in data:
+	#	times.append(santize(item))
+	times=[santize(item) for item in data]
 	jsjm.append(times)
 	#nester.print_list(data)
-print(jsjm)
-
-			
-
-
-
+#print(jsjm)
+new_jsjm=[]
+#distance=[]
+new_jsjm=[sorted(set(item)) for item in jsjm]
+#print(distance)
+print([item[0:3] for item in new_jsjm])
+print(new_jsjm)
+names2=["james2.txt","mikey2.txt","julie2.txt","sarah2.txt"]
+jsjm2=[]
+for name in names2:
+	times=[]
+	times=[santize(item) for item in fileTolist(name)]
+	jsjm2.append(times)
+print(jsjm2)
+name_data={}
+for item in jsjm2:
+	data['Name']=item.pop(0)
+	data['DOB']=item.pop(0)
+	data['Times']=item
+	name_data.append(data)
+print(name_data)
